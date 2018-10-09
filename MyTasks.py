6149df67509edf98e267c3f9d790e30df53c2c73
ldv10 <ninleo10@hotmail.com>
2018-10-09 17:58:42 -0600

    Update MyTasks.py

--- MyTasks.py
+++ MyTasks.py
@@ -4,11 +4,12 @@ import string
 class MyTasks(TaskSet):
     @task
     def simple_one(self):
-        my_respone = dict()
+        my_respone = ""
         with self.client.get('/', name='Test 1', catch_response=True) as response:
             try:
-                my_respone = response.json()
+                my_respone = response
             except ValueError as e:
                 response.failure('Could not get a response in json')
                 return
             response.success()
+
