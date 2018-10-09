078f8de6dbd39e58a4c840ad1ae533b8f82ae4aa
ldv10 <ninleo10@hotmail.com>
2018-10-09 17:45:04 -0600

    Update MyTasks.py

--- MyTasks.py
+++ MyTasks.py
@@ -5,7 +5,7 @@ class MyTasks(TaskSet):
     @task
     def simple_one(self):
         my_respone = dict()
-        with self.client.get('/', name='Test 1', catch_respone=True) as response:
+        with self.client.get('/', name='Test 1', catch_response=True) as response:
             try:
                 my_respone = response.json()
             except ValueError as e:
