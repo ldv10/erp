21c81f530bd66fa7ac72987eb44c1b900c89444e
ldv10 <ninleo10@hotmail.com>
2018-10-09 17:18:14 -0600

    Create MyTask.py

---
+++ MyTask.py
@@ -0,0 +1,14 @@
+from locust import TaskSet, task
+import random
+import string
+ class MyTasks(TaskSet):
+    @task
+    def simple_one(self):
+        my_respone = dict()
+        with self.client.get('/', name='Test 1', catch_respone=True) as response:
+            try:
+                my_respone = response.json()
+            except ValueError as e:
+                response.failure('Could not get a response in json')
+                return
+            response.success()
