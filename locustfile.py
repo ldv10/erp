753007550c3a70a47bd3ec573a52c6b5d901a2aa
ldv10 <ninleo10@hotmail.com>
2018-10-09 17:45:23 -0600

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,6 @@
 from locust import HttpLocust
 from tasks import MyTasks
- class MyLocust(HttpLocust):
+class MyLocust(HttpLocust):
     task_set = MyTasks
     min_wait = 5000
     max_wait = 15000
